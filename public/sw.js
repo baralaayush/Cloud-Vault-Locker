a1b51f8e84a062c06c46da2279c9bcb13400124c
Aayush Baral <aayushbaral.abb@gmail.com>
2026-08-08 11:55:41 +0545

    new version of previously PortalHub app commit

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,8 @@
 const CACHE_NAME = 'locker-v1';
 const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './favicon.svg',
-  './icon-192.svg',
-  './icon-512.svg'
+  '/',
+  '/index.html',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
